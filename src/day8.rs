3a9200e4a750a2ede6b0999a3d1df9c38add4813
Jason Ozias <jason.ozias@gmail.com>
2017-12-09 12:16:04 -0500

    AoC Day 8 - One Star

--- src/day8.rs
+++ src/day8.rs
@@ -61,11 +61,13 @@ struct Condition {
     /// The operater, e.g. `<`, `>`
     op: Operator,
     /// The value on the right of the condition.
-    value: u32,
+    value: i32,
 }
 
 /// A complete register command.
 struct RegisterCommand {
+    /// The name of the register to operate on.
+    register: String,
     /// The command (inc or dec)
     command: Command,
     /// The value to use with the command.
@@ -81,15 +83,23 @@ pub fn largest_register_value<T: BufRead>(reader: T, _second_star: bool) -> Resu
 
     for line_result in reader.lines() {
         let line = &line_result.unwrap_or_else(|_| "".to_string());
-        generate_register_map_entry(line, &mut register_map, &mut commands)?;
+        generate_register_map_entry_and_command(line, &mut register_map, &mut commands)?;
     }
-    Ok(0)
+
+    for command in &commands {
+        if check_condition(&register_map, &command.condition)? {
+            execute_command(&mut register_map, command)?
+        }
+    }
+
+    let max = register_map.values().max().ok_or("No max found")?;
+    Ok(TryFrom::try_from(*max)?)
 }
 
-/// Generate a register map entry
-fn generate_register_map_entry(
+/// Generate a register map entry and the associated command
+fn generate_register_map_entry_and_command(
     line: &str,
-    register_map: &mut HashMap<String, u32>,
+    register_map: &mut HashMap<String, i32>,
     commands: &mut Vec<RegisterCommand>,
 ) -> Result<()> {
     let line_desc: Vec<&str> = line.split_whitespace().collect();
@@ -103,36 +113,76 @@ fn generate_register_map_entry(
     let command_register_str = line_desc.get(4).ok_or("Invalid command register!")?;
     let command_register = String::from(*command_register_str);
     let operator_str = line_desc.get(5).ok_or("Invalid operator!")?;
+    let condition_value = line_desc
+        .get(6)
+        .ok_or("Invalid condition value!")?
+        .parse::<i32>()?;
 
-    register_map.entry(name).or_insert(0);
+    register_map.entry(name.clone()).or_insert(0);
 
     commands.push(RegisterCommand {
+        register: name,
         command: TryFrom::try_from(*command_str)?,
         value: value,
         condition: Condition {
             register: command_register,
             op: TryFrom::try_from(*operator_str)?,
-            value: 1,
+            value: condition_value,
         },
     });
 
     Ok(())
 }
 
+/// Check the command condition
+fn check_condition(register_map: &HashMap<String, i32>, condition: &Condition) -> Result<bool> {
+    let register_value = register_map
+        .get(&condition.register)
+        .ok_or("Cannot read value from register")?;
+    let condition_value = &condition.value;
+
+    match condition.op {
+        Operator::GreaterThan => Ok(register_value > condition_value),
+        Operator::GreaterThanEqualTo => Ok(register_value >= condition_value),
+        Operator::EqualTo => Ok(register_value == condition_value),
+        Operator::LessThanEqualTo => Ok(register_value <= condition_value),
+        Operator::LessThan => Ok(register_value < condition_value),
+        Operator::NotEqualTo => Ok(register_value != condition_value),
+    }
+}
+
+/// Execute the given command
+fn execute_command(
+    register_map: &mut HashMap<String, i32>,
+    register_command: &RegisterCommand,
+) -> Result<()> {
+    let map_entry = register_map
+        .entry(register_command.register.clone())
+        .or_insert(0);
+
+    match register_command.command {
+        Command::Inc => *map_entry += register_command.value,
+        Command::Dec => *map_entry -= register_command.value,
+    }
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod test {
-    use super::{generate_register_map_entry, RegisterCommand};
+    use super::{check_condition, execute_command, generate_register_map_entry_and_command,
+                RegisterCommand};
     use error::Result;
     use std::collections::HashMap;
 
     fn generate_map_entry(
-        register_map: &mut HashMap<String, u32>,
+        register_map: &mut HashMap<String, i32>,
         commands: &mut Vec<RegisterCommand>,
     ) -> Result<()> {
-        generate_register_map_entry("b inc 5 if a > 1", register_map, commands)?;
-        generate_register_map_entry("a inc 1 if b < 5", register_map, commands)?;
-        generate_register_map_entry("c dec -10 if a >= 1", register_map, commands)?;
-        generate_register_map_entry("c inc -20 if c == 1", register_map, commands)?;
+        generate_register_map_entry_and_command("b inc 5 if a > 1", register_map, commands)?;
+        generate_register_map_entry_and_command("a inc 1 if b < 5", register_map, commands)?;
+        generate_register_map_entry_and_command("c dec -10 if a >= 1", register_map, commands)?;
+        generate_register_map_entry_and_command("c inc -20 if c == 10", register_map, commands)?;
         Ok(())
     }
 
@@ -146,5 +196,22 @@ mod test {
         assert_eq!(*register_map.get(&"a".to_string()).ok_or("").expect(""), 0);
         assert_eq!(*register_map.get(&"b".to_string()).ok_or("").expect(""), 0);
         assert_eq!(*register_map.get(&"c".to_string()).ok_or("").expect(""), 0);
+
+        let mut maximum_attained = ::std::i32::MIN;
+
+        for command in &commands {
+            if check_condition(&register_map, &command.condition).expect("") {
+                execute_command(&mut register_map, command).expect("");
+            }
+            let max = register_map.values().max().ok_or("No max found").expect("");
+
+            if *max > maximum_attained {
+                maximum_attained = *max;
+            }
+        }
+
+        let max = register_map.values().max().ok_or("No max found").expect("");
+        assert_eq!(*max, 1);
+        assert_eq!(maximum_attained, 10);
     }
 }
