19205e4d18125765fb78d8b4ab6c19c39f617e3a
Jason Ozias <jason.ozias@gmail.com>
2017-12-12 01:26:44 -0500

    AoC Day 12 - Two Stars

--- src/year2017/day12.rs
+++ src/year2017/day12.rs
@@ -5,16 +5,28 @@ use std::convert::TryFrom;
 use std::io::BufRead;
 
 /// Find the solution for Advent of Code 2017
-pub fn find_solution<T: BufRead>(reader: T, _second_star: bool) -> Result<u32> {
+pub fn find_solution<T: BufRead>(reader: T, second_star: bool) -> Result<u32> {
     let mut group_map = HashMap::new();
-    let mut group_zero = HashSet::new();
+
     for line_result in reader.lines() {
         let line = &line_result.unwrap_or_else(|_| "".to_string());
         parse_and_add(line, &mut group_map)?;
     }
 
-    get_and_add(0, &group_map, &mut group_zero)?;
-    Ok(TryFrom::try_from(group_zero.len())?)
+    if second_star {
+        let mut groups: Vec<HashSet<u32>> = Vec::new();
+        let group_map_clone = group_map.clone();
+
+        for k in group_map.keys() {
+            add_to_groups(*k, &group_map_clone, &mut groups)?;
+        }
+
+        Ok(TryFrom::try_from(groups.len())?)
+    } else {
+        let mut group_zero = HashSet::new();
+        get_and_add(0, &group_map, &mut group_zero)?;
+        Ok(TryFrom::try_from(group_zero.len())?)
+    }
 }
 
 /// Parse the line and add to group map.
@@ -35,16 +47,35 @@ fn parse_and_add(line: &str, group_map: &mut HashMap<u32, Vec<u32>>) -> Result<(
 }
 
 /// Get and add
-fn get_and_add(group: u32, group_map: &HashMap<u32, Vec<u32>>, group_zero: &mut HashSet<u32>) -> Result<()> {
+fn get_and_add(group: u32, group_map: &HashMap<u32, Vec<u32>>, group_set: &mut HashSet<u32>) -> Result<()> {
     let piped_tos = group_map.get(&group).ok_or("Group not found")?;
-    group_zero.insert(group);
+    group_set.insert(group);
 
     for piped_to in piped_tos {
-        if !group_zero.contains(piped_to) {
-            get_and_add(*piped_to, group_map, group_zero)?;
+        if !group_set.contains(piped_to) {
+            get_and_add(*piped_to, group_map, group_set)?;
         }
     }
 
+    Ok(())
+}
+
+/// Add to groups
+fn add_to_groups(group: u32, group_map: &HashMap<u32, Vec<u32>>, group_sets: &mut Vec<HashSet<u32>>) -> Result<()> {
+    let mut found = false;
+
+    for group_set in group_sets.iter() {
+        if group_set.contains(&group) {
+            found = true;
+            break;
+        }
+    }
+
+    if !found {
+        let mut new_set = HashSet::new();
+        get_and_add(group, group_map, &mut new_set)?;
+        group_sets.push(new_set);
+    }
     Ok(())
 }
 
@@ -71,8 +102,26 @@ mod one_star {
 
 #[cfg(test)]
 mod two_star {
+    use std::collections::{HashMap, HashSet};
+
     #[test]
     fn solution() {
-        assert!(true);
+        let mut group_map = HashMap::new();
+        super::parse_and_add("0 <-> 2", &mut group_map).expect("");
+        super::parse_and_add("1 <-> 1", &mut group_map).expect("");
+        super::parse_and_add("2 <-> 0, 3, 4", &mut group_map).expect("");
+        super::parse_and_add("3 <-> 2, 4", &mut group_map).expect("");
+        super::parse_and_add("4 <-> 2, 3, 6", &mut group_map).expect("");
+        super::parse_and_add("5 <-> 6", &mut group_map).expect("");
+        super::parse_and_add("6 <-> 4, 5", &mut group_map).expect("");
+
+        let mut groups: Vec<HashSet<u32>> = Vec::new();
+        let group_map_clone = group_map.clone();
+
+        for k in group_map.keys() {
+            super::add_to_groups(*k, &group_map_clone, &mut groups).expect("");
+        }
+
+        assert_eq!(groups.len(), 2);
     }
 }
