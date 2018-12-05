85bcd0ce4f143b4e0bef9fe638af559b6cccee64
Jason Ozias <jason.ozias@kroger.com>
2018-12-05 17:29:45 -0500

    WIP: 2018 Advent of Code - Day 05 - Star 1

--- src/year2018/day05.rs
+++ src/year2018/day05.rs
@@ -1,6 +1,45 @@
 use error::Result;
 use std::io::BufRead;
 
-pub fn find_solution<T: BufRead>(_reader: T, _second_star: bool) -> Result<u32> {
-    Ok(1)
+pub fn find_solution<T: BufRead>(reader: T, second_star: bool) -> Result<u32> {
+    for line_result in reader.lines() {
+        if let Ok(line) = line_result {
+            let mut ch_vec: Vec<char> = line.chars().collect();
+
+            for (idx, ch) in ch_vec.iter().enumerate() {
+
+            }
+        }
+    }
+
+    if second_star {
+        Ok(1)
+    } else {
+        Ok(1)
+    }
+}
+
+#[cfg(test)]
+mod one_star {
+    use super::find_solution;
+    use error::Result;
+    use std::io::Cursor;
+
+    const TEST_CHAIN: &str = "dabAcCaCBAcCcaDA";
+
+    #[test]
+    fn solution() -> Result<()> {
+        assert_eq!(find_solution(Cursor::new(TEST_CHAIN), false)?, 10);
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod two_star {
+    use error::Result;
+
+    #[test]
+    fn solution() -> Result<()> {
+        Ok(())
+    }
 }
