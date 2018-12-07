3026932f45d8d89532a6aac7a8a07df3f0aa14d8
Jason Ozias <jason.ozias@gmail.com>
2018-12-06 19:53:13 -0500

    2018 Advent of Code - Day 06 - Star 2

--- src/year2018/day06.rs
+++ src/year2018/day06.rs
@@ -5,7 +5,7 @@ use regex::Regex;
 use std::collections::{BTreeMap, HashMap};
 use std::io::BufRead;
 
-pub fn find_solution<T: BufRead>(reader: T, _second_star: bool) -> Result<u32> {
+pub fn find_solution<T: BufRead>(reader: T, second_star: bool) -> Result<u32> {
     let line_re = Regex::new(r#"(\d+), (\d+)"#)?;
     let mut coords: Vec<(i32, i32)> = Vec::new();
 
@@ -20,36 +20,52 @@ pub fn find_solution<T: BufRead>(reader: T, _second_star: bool) -> Result<u32> {
     }
 
     let (max_x, max_y) = max_coords(&coords);
-    let mut md_map = BTreeMap::new();
-    let mut on_boundary = IndexSet::new();
 
-    for y in 0..=max_y {
-        for x in 0..=max_x {
-            let closest = find_closest((x, y), &coords);
+    if second_star {
+        let d_to_check = if coords.len() == 6 { 32 } else { 10000 };
+        let mut less_than_d = 0;
+        for y in 0..=max_y {
+            for x in 0..=max_x {
+                let total_of_mds = total_of_mds((x, y), &coords);
 
-            if closest.len() == 1 {
-                md_map.insert((x, y), closest[0]);
-
-                if x == 0 || y == 0 || x == max_x || y == max_y {
-                    on_boundary.insert(closest[0]);
+                if total_of_mds < d_to_check {
+                    less_than_d += 1;
                 }
             }
         }
+        Ok(less_than_d)
+    } else {
+        let mut md_map = BTreeMap::new();
+        let mut on_boundary = IndexSet::new();
+
+        for y in 0..=max_y {
+            for x in 0..=max_x {
+                let closest = find_closest((x, y), &coords);
+
+                if closest.len() == 1 {
+                    md_map.insert((x, y), closest[0]);
+
+                    if x == 0 || y == 0 || x == max_x || y == max_y {
+                        on_boundary.insert(closest[0]);
+                    }
+                }
+            }
+        }
+
+        let rest: BTreeMap<(i32, i32), (i32, i32)> = md_map
+            .iter()
+            .filter(|(_, closest)| !on_boundary.contains(*closest))
+            .map(|(x, v)| (*x, *v))
+            .collect();
+
+        let mut frequency: HashMap<(i32, i32), u32> = HashMap::new();
+        for (_, bounded_closest) in rest {
+            *frequency.entry(bounded_closest).or_insert(0) += 1;
+        }
+
+        let max = frequency.iter().max_by_key(|(_, x)| *x).map(|(_, x)| *x).ok_or_else(|| "no maximum")?;
+        Ok(max)
     }
-
-    let rest: BTreeMap<(i32, i32), (i32, i32)> = md_map
-        .iter()
-        .filter(|(_, closest)| !on_boundary.contains(*closest))
-        .map(|(x, v)| (*x, *v))
-        .collect();
-
-    let mut frequency: HashMap<(i32, i32), u32> = HashMap::new();
-    for (_, bounded_closest) in rest {
-        *frequency.entry(bounded_closest).or_insert(0) += 1;
-    }
-
-    let max = frequency.iter().max_by_key(|(_, x)| *x).map(|(_, x)| *x).ok_or_else(|| "no maximum")?;
-    Ok(max)
 }
 
 fn find_closest(point: (i32, i32), coords: &[(i32, i32)]) -> Vec<(i32, i32)> {
@@ -66,6 +82,11 @@ fn find_closest(point: (i32, i32), coords: &[(i32, i32)]) -> Vec<(i32, i32)> {
         }
     }
     result
+}
+
+fn total_of_mds(point: (i32, i32), coords: &[(i32, i32)]) -> u32 {
+    let sum: i32 = coords.iter().map(|coord| manhattan_distance(point, *coord)).sum();
+    sum as u32
 }
 
 /// Calculate the manhattan distance between two (x,y) tuples.
@@ -101,20 +122,20 @@ mod one_star {
 
 #[cfg(test)]
 mod two_star {
-    // use super::find_solution;
+    use super::find_solution;
     use error::Result;
-    // use std::io::Cursor;
+    use std::io::Cursor;
 
-    //     const TEST_CHAIN: &str = r"1, 1
-    // 1, 6
-    // 8, 3
-    // 3, 4
-    // 5, 5
-    // 8, 9";
+    const TEST_CHAIN: &str = r"1, 1
+    1, 6
+    8, 3
+    3, 4
+    5, 5
+    8, 9";
 
     #[test]
     fn solution() -> Result<()> {
-        // assert_eq!(find_solution(Cursor::new(TEST_CHAIN), true)?, 4);
+        assert_eq!(find_solution(Cursor::new(TEST_CHAIN), true)?, 16);
         Ok(())
     }
 }
