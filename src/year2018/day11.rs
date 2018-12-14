6e995d83a2e78c6fb35b65b0d100e915f43b0044
Jason Ozias <jason.ozias@kroger.com>
2018-12-13 19:28:40 -0500

    WIP: 2018 Advent of Code - Day 11 - Star 2

--- src/year2018/day11.rs
+++ src/year2018/day11.rs
@@ -1,112 +1,114 @@
 //! Advent of Code - Day 11 "Chronal Charge" Solution
 use error::Result;
-use std::collections::HashMap;
+use ndarray::Array2;
+use std::convert::TryFrom;
 use std::io::BufRead;
 
 pub fn find_solution<T: BufRead>(reader: T, second_star: bool) -> Result<u32> {
-    let mut pl_map: HashMap<(isize, isize), isize> = HashMap::new();
-
     let mut serial_number = 0;
 
     for line in reader.lines().filter_map(|x| x.ok()) {
-        serial_number = line.parse::<isize>()?;
+        serial_number = line.parse::<usize>()?;
     }
 
-    for y in 1..=300 {
-        for x in 1..=300 {
-            pl_map.insert((x, y), find_cell_power(x, y, serial_number)?);
-        }
-    }
-
-    if second_star {
-        let mut result_map: HashMap<(isize, isize, isize), isize> = HashMap::new();
-
-        for size in 1..=300 {
-            println!("Checking size {}", size);
-            for y in 1..=(300 - size + 1) {
-                for x in 1..=(300 - size + 1) {
-                    find_sum_at_size(x, y, size, &pl_map, &mut result_map)?;
-                }
-            }
-        }
-        let (max_x, max_y, max_size) = result_map.iter().max_by_key(|(_, pl)| *pl).map(|((x, y, size), _)| (*x, *y, *size)).ok_or_else(|| "")?;
-        println!("{},{},{}", max_x, max_y, max_size);
-    } else {
-        let mut result_map: HashMap<(isize, isize), isize> = HashMap::new();
-
-        for y in 1..=298 {
-            for x in 1..=298 {
-                result_map.insert((x, y), find_3x3_sum(x, y, &pl_map)?);
-            }
-        }
-        let (max_x, max_y) = result_map.iter().max_by_key(|(_, pl)| *pl).map(|((x, y), _)| (*x, *y)).ok_or_else(|| "")?;
-        println!("{},{}", max_x, max_y);
-    }
+    let result = submatrix_sum_queries(serial_number, second_star)?;
+    println!("{},{},{} with power level {}", result.0, result.1, result.2, result.3);
 
     Ok(0)
 }
 
-fn find_cell_power(x: isize, y: isize, serial_number: isize) -> Result<isize> {
+fn find_cell_power(x: usize, y: usize, serial_number: usize) -> Result<isize> {
     let rack_id = x + 10;
-    let mut power_level = rack_id * y;
-    power_level += serial_number;
-    power_level *= rack_id;
+    let mut power_level = isize::try_from(rack_id * y)?;
+    power_level += isize::try_from(serial_number)?;
+    power_level *= isize::try_from(rack_id)?;
     power_level = if power_level > 100 { (power_level / 100) % 10 } else { 0 };
     power_level -= 5;
     Ok(power_level)
 }
 
-fn find_3x3_sum(x: isize, y: isize, pl_map: &HashMap<(isize, isize), isize>) -> Result<isize> {
-    let mut power_level = 0;
-    for ny in y..y + 3 {
-        for nx in x..x + 3 {
-            power_level += pl_map.get(&(nx, ny)).ok_or_else(|| "")?;
-        }
-    }
-    Ok(power_level)
-}
+fn submatrix_sum_query(aux: &Array2<isize>, size: usize, max_power_level: &mut isize, max_cell: &mut (usize, usize, usize, isize)) {
+    for i in 0..(300 - size) {
+        for j in 0..(300 - size) {
+            let result = sum_query(&aux, i, j, size);
 
-fn find_sum_at_size(x: isize, y: isize, size: isize, pl_map: &HashMap<(isize, isize), isize>, result_map: &mut HashMap<(isize, isize, isize), isize>) -> Result<()> {
-    if size == 1 {
-        for ny in y..y + size {
-            for nx in x..x + size {
-                result_map.insert((x, y, size), *pl_map.get(&(nx, ny)).ok_or_else(|| "")?);
+            if result > *max_power_level {
+                *max_power_level = result;
+                max_cell.0 = i;
+                max_cell.1 = j;
+                max_cell.2 = size + 1;
+                max_cell.3 = result;
             }
         }
-    } else {
-        // println!("checking {},{} at {}", x, y, size);
-        let previous_map = result_map.clone();
-        let mut power_level = *previous_map.get(&(x, y, size - 1)).ok_or_else(|| "no prev")?;
+    }
+}
 
-        // println!("prev pl: {}", power_level);
-        // Add the last column
-        let last_x = x + size - 1;
+fn submatrix_sum_queries(serial_number: usize, second_star: bool) -> Result<(usize, usize, usize, isize)> {
+    let mut power_level: Array2<isize> = Array2::zeros((300, 300));
 
-        for ny in y..y + size {
-            power_level += *pl_map.get(&(last_x, ny)).ok_or_else(|| "bad col")?;
+    for i in 0..300 {
+        for j in 0..300 {
+            power_level[[i, j]] = find_cell_power(i, j, serial_number)?;
         }
-
-        // println!("after last col: {}", power_level);
-        // Add the bottom row - last cell (included in last column calc)
-        let last_y = y + size - 1;
-
-        for nx in x..x + size - 1 {
-            power_level += *pl_map.get(&(nx, last_y)).ok_or_else(|| "bad row")?;
-        }
-
-        // println!("after last row: {}", power_level);
-
-        result_map.insert((x, y, size), power_level);
     }
 
-    Ok(())
+    let mut aux: Array2<isize> = Array2::zeros((300, 300));
+
+    for i in 0..300 {
+        aux[[0, i]] = power_level[[0, i]];
+    }
+
+    for i in 1..300 {
+        for j in 0..300 {
+            aux[[i, j]] = power_level[[i, j]] + aux[[i - 1, j]];
+        }
+    }
+
+    for i in 0..300 {
+        for j in 1..300 {
+            aux[[i, j]] += aux[[i, j - 1]];
+        }
+    }
+
+    let mut max_power_level = isize::min_value();
+    let mut max_cell = (0, 0, 0, 0);
+
+    if second_star {
+        for size in 0..300 {
+            println!("Checking size {}", size);
+            submatrix_sum_query(&aux, size, &mut max_power_level, &mut max_cell);
+        }
+    } else {
+        submatrix_sum_query(&aux, 2, &mut max_power_level, &mut max_cell);
+    }
+
+    Ok(max_cell)
+}
+
+fn sum_query(aux: &Array2<isize>, i: usize, j: usize, size: usize) -> isize {
+    let tli = i;
+    let tlj = j;
+    let rbi = i + size;
+    let rbj = j + size;
+    let mut res = aux[[rbi, rbj]];
+
+    if tli > 0 {
+        res -= aux[[tli - 1, rbj]];
+    }
+    if tlj > 0 {
+        res -= aux[[rbi, tlj - 1]];
+    }
+    if tli > 0 && tlj > 0 {
+        res += aux[[tli - 1, tlj - 1]];
+    }
+
+    res
 }
 
 #[cfg(test)]
 mod one_star {
-    use super::{find_cell_power, find_solution};
+    use super::{find_cell_power, submatrix_sum_queries};
     use error::Result;
-    use std::io::Cursor;
 
     #[test]
     fn solution() -> Result<()> {
@@ -114,22 +116,23 @@ mod one_star {
         assert_eq!(find_cell_power(122, 79, 57)?, -5);
         assert_eq!(find_cell_power(217, 196, 39)?, 0);
         assert_eq!(find_cell_power(101, 153, 71)?, 4);
-        assert_eq!(find_solution(Cursor::new("18"), false)?, 0);
-        assert_eq!(find_solution(Cursor::new("42"), false)?, 0);
+        assert_eq!(submatrix_sum_queries(18, false)?, (33, 45, 3, 29));
+        assert_eq!(submatrix_sum_queries(42, false)?, (21, 61, 3, 30));
+        assert_eq!(submatrix_sum_queries(7511, false)?, (21, 22, 3, 34));
         Ok(())
     }
 }
 
 #[cfg(test)]
 mod two_star {
-    use super::find_solution;
+    use super::submatrix_sum_queries;
     use error::Result;
-    use std::io::Cursor;
 
     #[test]
     fn solution() -> Result<()> {
-        assert_eq!(find_solution(Cursor::new("18"), true)?, 0);
-        assert_eq!(find_solution(Cursor::new("42"), true)?, 0);
+        assert_eq!(submatrix_sum_queries(18, true)?, (90, 269, 16, 113));
+        assert_eq!(submatrix_sum_queries(42, true)?, (232, 251, 12, 119));
+        assert_eq!(submatrix_sum_queries(7511, true)?, (236, 287, 13, 0));
         Ok(())
     }
 }
